9946cc42cc2287114fab54e7165c01b51a13c4bd
Andreas Dzialocha <x12@adz.garden>
2022-11-14 18:37:11 +0100

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "it8951-video",
+    about = "Play videos on IT8951-controlled e-paper displays"
+)]
+struct Opt {
+    /// Input video file.
+    #[structopt(parse(from_os_str))]
+    video: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }

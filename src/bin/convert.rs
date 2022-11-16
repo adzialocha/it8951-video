9d54d2ee75bd4dab76bee78fd865e0b1fc308d00
Andreas Dzialocha <x12@adz.garden>
2022-11-16 22:35:03 +0100

    Faster conversion method, show progress bar

--- src/bin/convert.rs
+++ src/bin/convert.rs
@@ -1,12 +1,16 @@
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
+use ffmpeg_next::format::{input, Pixel};
+use ffmpeg_next::media::Type;
+use ffmpeg_next::software::scaling::{context::Context, flag::Flags};
+use ffmpeg_next::util::frame::video::Video;
 use image::imageops::BiLevel;
-use image::{Rgb, RgbImage};
+use image::{GrayImage, Luma};
 use itertools::Itertools;
+use pbr::ProgressBar;
 use structopt::StructOpt;
-use video_rs::{Decoder, Locator};
 
 use it8951_video::RawFrames;
 
@@ -33,69 +37,99 @@ struct Opt {
     height: u32,
 }
 
-fn extract_video_frames(
-    file_path: PathBuf,
+fn extract_video_frames<P>(
+    file_path: &P,
     target_width: u32,
     target_height: u32,
-) -> Result<RawFrames> {
-    let source = Locator::from(file_path);
-    let mut decoder = Decoder::new(&source)?;
+) -> Result<RawFrames>
+where
+    P: AsRef<Path>,
+{
+    let mut frames = Vec::new();
 
-    let data = decoder
-        .decode_iter()
-        .take_while(Result::is_ok)
-        .map(|frame| frame.unwrap())
-        .enumerate()
-        .map(|(index, (_, raw_frame))| {
-            println!("Convert frame {}", index + 1);
+    // Find video stream in file
+    let mut context_video = input(file_path)?;
+    let input = context_video
+        .streams()
+        .best(Type::Video)
+        .ok_or(ffmpeg_next::Error::StreamNotFound)?;
+    let video_stream_index = input.index();
 
-            let size = raw_frame.shape();
-            let width = size[0] as u32;
-            let height = size[1] as u32;
+    // Prepare video decoder
+    let context_decoder =
+        ffmpeg_next::codec::context::Context::from_parameters(input.parameters())?;
+    let mut decoder = context_decoder.decoder().video()?;
 
-            // Load frame data into image buffer
-            let image = RgbImage::from_fn(height, width, |x, y| {
-                let rgb = raw_frame
-                    .slice(ndarray::s![y as usize, x as usize, ..])
-                    .to_slice()
-                    .unwrap();
-                Rgb([rgb[0], rgb[1], rgb[2]])
-            });
+    // Prepare scaler which resizes the frames and converts them to grayscale
+    let mut scaler = Context::get(
+        decoder.format(),
+        decoder.width(),
+        decoder.height(),
+        Pixel::GRAY8,
+        target_width,
+        target_height,
+        Flags::BILINEAR,
+    )?;
 
-            // Resize image to make it cover whole display
-            let resized_image = image::imageops::resize(
-                &image,
-                target_width,
-                target_height,
-                image::imageops::FilterType::Nearest,
-            );
+    let mut pb = ProgressBar::new(input.frames() as u64);
+    pb.format("╢▌▌░╟");
+    let mut frame_index = 0;
 
-            // Apply b/w dithering on image buffer
-            let mut grayscale_image = image::imageops::grayscale(&resized_image);
-            image::imageops::colorops::dither(&mut grayscale_image, &BiLevel);
+    let mut receive_and_process_decoded_frames =
+        |decoder: &mut ffmpeg_next::decoder::Video| -> Result<(), ffmpeg_next::Error> {
+            let mut decoded = Video::empty();
 
-            // Convert to raw format, representing black (0) or white (1) pixels in an array
-            let iter = grayscale_image.enumerate_pixels();
-            let mut data: Vec<u8> = Vec::new();
-            let base: u8 = 2;
+            while decoder.receive_frame(&mut decoded).is_ok() {
+                // Decode next frame
+                let mut frame = Video::empty();
+                scaler.run(&decoded, &mut frame)?;
 
-            for chunk in &iter.chunks(8) {
-                let mut byte: u8 = 0b0000_0000;
-
-                chunk.enumerate().for_each(|(index, (_, _, luma))| {
-                    if luma.0[0] == 255 {
-                        byte |= base.pow(index as u32);
-                    }
+                // Encode as grayscale image
+                let frame_data = frame.data(0);
+                let mut img = GrayImage::from_fn(frame.width(), frame.height(), |x, y| {
+                    let luma = frame_data[(y as usize * frame.width() as usize) + x as usize];
+                    Luma::from([luma])
                 });
 
-                data.push(byte);
+                // Apply dithering filter
+                image::imageops::colorops::dither(&mut img, &BiLevel);
+
+                // Convert to raw format, representing black (0) or white (1) pixels in an array
+                let iter = img.enumerate_pixels();
+                let mut data: Vec<u8> = Vec::new();
+                let base: u8 = 2;
+
+                for chunk in &iter.chunks(8) {
+                    let mut byte: u8 = 0b0000_0000;
+
+                    chunk.enumerate().for_each(|(index, (_, _, luma))| {
+                        if luma.0[0] == 255 {
+                            byte |= base.pow(index as u32);
+                        }
+                    });
+
+                    data.push(byte);
+                }
+
+                frames.push(data);
+                frame_index += 1;
+                pb.inc();
             }
 
-            data
-        })
-        .collect();
+            Ok(())
+        };
 
-    Ok(RawFrames::new(target_width, target_height, data))
+    for (stream, packet) in context_video.packets() {
+        if stream.index() == video_stream_index {
+            decoder.send_packet(&packet)?;
+            receive_and_process_decoded_frames(&mut decoder)?;
+        }
+    }
+
+    decoder.send_eof()?;
+    receive_and_process_decoded_frames(&mut decoder)?;
+
+    Ok(RawFrames::new(target_width, target_height, frames))
 }
 
 fn write_raw_frames(file_path: PathBuf, frames: &RawFrames) -> Result<()> {
@@ -108,13 +142,14 @@ fn main() -> Result<()> {
     let opt = Opt::from_args();
 
     println!(
-        r#"
+        r#"► Convert video for e-paper
+
 Video Dimensions: {}x{}
         "#,
         opt.width, opt.height
     );
 
-    let frames = extract_video_frames(opt.video.clone(), opt.width, opt.height)?;
+    let frames = extract_video_frames(&opt.video, opt.width, opt.height)?;
     write_raw_frames(opt.output, &frames)?;
     Ok(())
 }
